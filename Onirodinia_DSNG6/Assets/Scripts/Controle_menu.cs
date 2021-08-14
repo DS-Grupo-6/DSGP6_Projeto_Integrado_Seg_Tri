0d6e7025f1c9d709c0611c2eb09670a2928bc718
Virginia Leticia <vlafonso@hotmail.com>
2021-08-14 14:02:37 -0300

    Add fundo menu e botao sair jogo

--- Onirodinia_DSNG6/Assets/Scripts/Controle_menu.cs
+++ Onirodinia_DSNG6/Assets/Scripts/Controle_menu.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Botao_iniciar : MonoBehaviour
+public class Controle_menu : MonoBehaviour
 {
     public void IniciarFase1(){ 
         UnityEngine.SceneManagement.SceneManager.LoadScene("CutScene1");
-    }    
+    }
+    
+    public void FecharJogo()
+    {
+        Application.Quit();
+    } 
 }
